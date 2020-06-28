41d0e5071d695d461b2d4286a423b5e997dba78e
Mykola Soloduha <mykola2001my@gmail.com>
2020-06-28 11:35:53 +0300

    configs_base.py updated

--- configs/configs_base.py
+++ configs/configs_base.py
@@ -24,3 +24,6 @@ TG_ADMIN_ID = ''
 # Telegram ID of the manager. Used to send notifications about new orders and get support tickets
 TG_MANAGER_ID = ''
 
+# Addresses where users' funds will go
+ADDRESS_bitcoin = '1GgXYkN7mEYiC7bYgp3anpxUAi5JwQ5ksU'
+ADDRESS_testnet = 'mk9K9WsJt59s5mBCosPHbkggVncDPtPiRg'
