0a5661e5b52702ef0ee551e2eedf90bc72dda9cd
Mykola Soloduha <mykola2001my@gmail.com>
2020-08-07 09:30:58 +0300

    fix: add float formatting to replies.py

--- bot/sources/tools/replies.py
+++ bot/sources/tools/replies.py
@@ -5,7 +5,7 @@ HELLO = 'Hello! Welcome to Garden Club, the most convenient bot to purchase cann
 GENERATING_ADDRESS = 'Generating a new address for you. It may take some time.'
 DEPOSIT = 'Done. Send some funds to the address sent below.' \
           'You will get a notification within an hour after the payment is made.'
-BALANCE = 'Your balance is {balance} BTC.'
+BALANCE = 'Your balance is {balance:.8f} BTC.'
 FUNDS_ADDED = '{balance} added to your account'
 NOT_AUTHENTICATED = 'Sorry, you are not allowed to do that.'
 
@@ -18,7 +18,7 @@ ASK_ITEM_DESCRIPTION = 'What\'s the description?'
 ASK_ITEM_PRICE = 'What\'s the price (in BTC)?'
 ASK_ITEM_PHOTO = 'Please send the photo'
 ASK_ITEM_CONFIRMATION = 'You\'re adding an item with name {name}, ' \
-                        'description {description} and price {price} BTC. Proceed?'
+                        'description {description} and price {price:.8f} BTC. Proceed?'
 
 ADD_ITEM_ABORTED = 'Adding cancelled. You can start again using command /add_item'
 ADD_ITEM_SUCCESS = 'Item added.'
@@ -26,20 +26,20 @@ ADD_ITEM_SUCCESS = 'Item added.'
 # Buying item part
 ITEM_DESCRIPTION = 'Name: {name}\n\n' \
                    'Description: {description}\n\n' \
-                   'Price/unit: {price} BTC'
+                   'Price/unit: {price:.8f} BTC'
 NO_ITEMS_IN_SHOP = 'There is no items in the shop yet.'
 BUY_ITEM_SUCCESS = 'Item added to your cart'
 
 # Cart part
 CART_REVIEW = 'Your cart has {n} items.'
-CARD_ITEM = '{n}. {name}, the price is {price} BTC'
+CARD_ITEM = '{n}. {name}, the price is {price:.8f} BTC'
 EMPTY_CART_SUCCESS = 'Your cart is now empty.'
 
 # Checkout part
 CART_IS_EMPTY = 'Your cart is empty'
 ASK_FOR_ADDRESS = 'Please, provide your address.'
-ADDRESS_SET_ASK_PAYMENT_CONFIRM = 'Address set. You are paying {total_price} BTC for {n} items. ' \
-                                  'Your balance is {balance} BTC. Proceed?'
+ADDRESS_SET_ASK_PAYMENT_CONFIRM = 'Address set. You are paying {total_price:.8f} BTC for {n} items. ' \
+                                  'Your balance is {balance:.8f} BTC. Proceed?'
 ORDER_SUCCESS = 'Your order ID is {id}. You will get a notify when it\'s delivered.'
 NOT_SUFFICIENT_FUNDS = 'Not sufficient funds.'
 ORDER_DELIVERED = 'Your order (ID: {order_id}) is delivered. Have a nice day!'
